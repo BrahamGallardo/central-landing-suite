9b3f7c63fe244e9cee0dc952935a6021d5693293
BrahamGallardo <braham.gc@gmail.com>
2024-06-19 00:54:31 -0600

    Add Interceptors
    -AuthInterceptor
    
    -HttpErrorInterceptor
    
    -HttpResponseInterceptor
    
    -SpinnerInterceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,20 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { loadingSpinnerInterceptor } from './core/interceptors/loading-spinner.interceptor';
 import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { httpErrorInterceptor } from './core/interceptors/http-error.interceptor';
+import { loggingInterceptorFunctional, responseTimeInterceptorFunctional } from './core/interceptors/http-response.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(),
     provideHttpClient(),
+    provideHttpClient(withInterceptors([loadingSpinnerInterceptor])),
     provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withInterceptors([httpErrorInterceptor])),
+    provideHttpClient(withInterceptors([loggingInterceptorFunctional])),
+    provideHttpClient(withInterceptors([responseTimeInterceptorFunctional])),
   ]
 };
--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -1,34 +1,19 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
-import { inject } from '@angular/core';
-import { NgxSpinnerService } from 'ngx-spinner';
+import { HttpInterceptorFn } from '@angular/common/http';
 import { AuthService } from '../services/auth.service';
-import { Observable, finalize } from 'rxjs';
+import { inject } from '@angular/core';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-
-  const SHOW_SPINNER_HEADER = 'show-spinner';
-  const AUTHORIZATION_HEADER = 'Authorization';
-
-  const _spinner: NgxSpinnerService = inject(NgxSpinnerService);
   const _authService: AuthService = inject(AuthService);
-
-  const showSpinner = req.headers.get(SHOW_SPINNER_HEADER) !== 'false';
-
-  if (showSpinner) {
-    _spinner.show();
-  }
-
-  let modifiedReq = req.headers.has(SHOW_SPINNER_HEADER)
-    ? req.clone({ headers: req.headers.delete(SHOW_SPINNER_HEADER) })
-    : req;
 
   const token = _authService.getCurrentAuth() ? _authService.getCurrentAuth().token : null;
   if (token) {
-    modifiedReq = modifiedReq.clone({
-      headers: modifiedReq.headers.set(AUTHORIZATION_HEADER, `Bearer ${ token }`)
+    const authRequest = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${ token }`
+      }
     });
+    return next(authRequest);
+  } else {
+    return next(req);
   }
-  return next(modifiedReq).pipe(
-    finalize(() => _spinner.hide())
-  );
 };
